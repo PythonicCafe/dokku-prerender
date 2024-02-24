f92de6841229998306578e1f3ea6c890117d1751
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2024-02-24 18:40:33 -0300

    Connects to chromium on IPv4

--- server.js
+++ server.js
@@ -29,13 +29,16 @@ const server = prerender({
     "--headless",
     "--disable-gpu",
     "--remote-debugging-port=9222",
+    "--remote-debugging-address=127.0.0.1",
     "--hide-scrollbars",
     "--blink-settings=imagesEnabled=false",
     "--disable-dev-shm-usage", // Avoid `net::ERR_INSUFFICIENT_RESOURCES`
   ],
   logRequests: process.env.PRERENDER_LOG_REQUESTS === "true",
   port: process.env.PORT || 3000,
+  browserDebuggingPort: 9222,
 });
+
 for (let plugin of ENABLED_PLUGINS) {
   if ((!plugin) in PLUGINS) {
     process.stderr.write(`ERROR: plugin ${plugin} not found.`);
